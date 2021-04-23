4726374262d712e9f31ec5070f1c9104d4587b2b
BailiMohamedAli <bailimohamedali@gmail.com>
2021-04-23 18:37:15 +0100

    added more conditiona redering to navbar coding for fun

--- index.js
+++ index.js
@@ -12,11 +12,23 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+const nav ={
+    local: ''
+}
 
 //routes
-app.get('/', (req, res) => res.render('pages/home'));
-app.get('/login', (req, res) => res.render('pages/login'));
-app.get('/register', (req, res) => res.render('pages/register'));
+app.get('/', (req, res) => {
+    nav.local = 'home';
+    res.render('pages/home', { nav : nav })
+});
+app.get('/login', (req, res) => {
+    nav.local = 'login';
+    res.render('pages/login', { nav : nav });
+});
+app.get('/register', (req, res) => {
+    nav.local = 'register';
+    res.render('pages/register', { nav : nav })
+});
 app.use('/user', require('./routes/user'));
 
 //listning to server
