c4b60026b56888b0f41795db7ce64e6cc048497b
BailiMohamedAli <bailimohamedali@gmail.com>
2021-04-23 17:09:41 +0100

    model setup

---
+++ config/model/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    salt: {
+        type: String,
+        required: true
+    },
+    hash:{
+        type: String,
+        required: true
+    }
+});
+
+const user = mongoose.model('User', userSchema);
+
+module.exports = user;
